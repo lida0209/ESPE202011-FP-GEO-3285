d0aa324db89a0ff6d7b4ac457679dc02a26f786b
lida0209 <lidamk99@hotmail.com>
2021-02-03 14:46:37 -0500

    WS10Functions

--- assignments/Unit2/HW09-EmbeddedStructures/src/hw09/embeddedstructures/HW09EmbeddedStructures.java
+++ assignments/Unit2/HW09-EmbeddedStructures/src/hw09/embeddedstructures/HW09EmbeddedStructures.java
@@ -5,6 +5,8 @@
  */
 package hw09.embeddedstructures;
 
+import java.util.Scanner;
+
 /**
  *
  * @author HP
@@ -16,6 +18,70 @@ public class HW09EmbeddedStructures {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Scanner scanner = new Scanner (System.in); 
+       int Operation1;
+       int Operation2; 
+       int multiplication; 
+       int sum; 
+       float divisions; 
+       int mod; 
+       int tablenum1; 
+       int tablenum2; 
+       char option;
+       
+     do {
+System.out.print("Enter the operation 1-->"); 
+Operation1 = scanner.nextInt (); 
+scanner.nextLine();
+System.out.print("Enter the operator 2-->"); 
+Operation2 = scanner.nextInt(); 
+scanner.nextLine();
+
+if ( Operation1 % 2 == 0 && Operation2 % 2 == 0 )
+        {
+            System.out.println( "It's even" );
+        }
+        else
+        {
+            System.out.println( "It's odd" );
+        }
+
+if(Operation1 % 2 == 0 && Operation2 % 2 == 0) {
+    multiplication =Operation1 + Operation2;
+    System.out.println("The multiplication of -->" + Operation1 + "*" + Operation2 + " = " + multiplication);
+    
+} else if (Operation1 % 3 == 0 && Operation2 % 3 == 0){
+    sum = Operation1 + Operation2; 
+    System.out.println("The sum of -->" + Operation1 + " + " + Operation2 + " = " + sum);
+  
+} else if (Operation1 % 7 == 0 && Operation2 % 7 == 0){
+mod = Operation1 * Operation2; 
+System.out.println("The module of -->" + Operation1 + "%" + Operation2 + "-" + mod);
+
+} else if (Operation1 % 11 == 0 && Operation2 % 11 == 0){
+for (int i = 1; i <= 12; i++) {
+tablenum1 = Operation1 * i; 
+System.out.println (Operation1 + ":*" + i + " = " + tablenum1 + "\n");
+
+}System.out.println("=================================================");
+for (int j = 1; j <= 12; j++) {
+tablenum2 = Operation2 * j; 
+System.out.println(Operation2 + " * " + j + " = " + tablenum2 + "\n");
+}
+
+} else if (Operation1 % 13 == 0 && Operation2 % 13 == 0){
+divisions = Operation1 / Operation2; 
+System.out.println("The division of --> " + Operation1 + "/" + Operation2 + " = " + divisions);
+
+} else{
+System.out.println("This operation does not exist");
+}
+System.out.println("Do you want to try again? Yes[y] or No[n] -->");
+option = scanner.nextLine().charAt(0); 
+    
+     }while (option != 'n'); 
+System.out.println("==== Goodbye friends ====");
     }
     
 }
---
+++ workshops/unit2/WS10Functions/src/ws10functions/WS10Functions.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws10functions;
+
+/**
+ *
+ * @author HP
+ */
+public class WS10Functions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       int divd = 5;
+        int divr = 10;
+        float quotient = 0;
+        int x = 0;
+        int y = 0;
+
+        //call to divideTwoNumbers() function
+        quotient = divideTwoNumbers(divd, divr);
+
+        System.out.println("Quotient of 5 and 10 is -> " + quotient); //0.5 or ?
+
+        y = f(3);
+        System.out.println("3^2 -> " + y);
+
+        y = f(-3);
+        System.out.println("-3^2 -> " + y); //9
+        
+        x = -1;
+        y = f(x);
+        System.out.println("the square of -> " + x + " <- is equal to -> " + y);
+
+    }
+
+    //function definition
+    //return data type    name     parameter1   parameter2
+    public static float divideTwoNumbers(int dividend, int divisor) {
+        //body of the function
+        float quotient = 0.0F;
+
+        //             5        10   -> 0.5
+        // float     int        int
+        quotient = (float) dividend / (float) divisor;
+
+        return quotient;
+    }
+
+    public static int f(int x) {
+        int y = 0;
+
+        y = x * x;
+
+        return y;
+    }
+
+    public static int g(int x) {
+        int y = 0;
+
+        y = x * x + 2 * x + 1;
+
+        return y;
+    }
+
+}
