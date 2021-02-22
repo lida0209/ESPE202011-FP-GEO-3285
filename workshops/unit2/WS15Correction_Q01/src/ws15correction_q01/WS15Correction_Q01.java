30eb5628e6501877cd6ddf17c7507963b6a3bf11
lida0209 <lidamk99@hotmail.com>
2021-02-22 14:18:33 -0500

    WS16_Matrix_addition

--- workshops/unit2/WS15Correction_Q01/src/ws15correction_q01/WS15Correction_Q01.java
+++ workshops/unit2/WS15Correction_Q01/src/ws15correction_q01/WS15Correction_Q01.java
@@ -16,6 +16,32 @@ public class WS15Correction_Q01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int totalOfPlayers = 0;
+        String[] players;
+        float[] goalAverages;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(" Please Enter the amount of players you want to record -> ");
+        totalOfPlayers = scanner.nextInt();
+
+        players = new String[totalOfPlayers];
+        goalAverages = new float[totalOfPlayers];
+
+        for (int i = 0; i < totalOfPlayers; i++) {
+            scanner.nextLine();
+            System.out.print(" Please Enter the name of player # " + i + " -> ");
+            players[i] = scanner.nextLine();
+            System.out.print(" Please enter " + players[i] + " goals average ->");
+            goalAverages[i] = scanner.nextFloat();
+        }
+
+        System.out.println(" Player    ->  \t     goal average ");
+
+        for (int i = 0; i < totalOfPlayers; i++) {
+            System.out.println(players[i] + " ->  \t " + goalAverages[i]);
+        }
+
     }
     
 }
--- workshops/unit2/WS16BidimensionalArrays/src/ws16bidimensionalarrays/WS16BidimensionalArrays.java
+++ workshops/unit2/WS16BidimensionalArrays/src/ws16bidimensionalarrays/WS16BidimensionalArrays.java
@@ -16,6 +16,46 @@ public class WS16BidimensionalArrays {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int a[][] = {{5, 7, 9}, {9, 5, 8}, {3, 2, 4}};
+        int b[][] = {{8, 3, 5}, {-5, 4, -3}, {9, 10, 24}};
+
+        //    5    7     9           8     3     5       13    10    14
+        //    9    5     8     +    -5     4    -3   =    4     9     5
+        //    3    2     4           9    10    24       12    12    28
+        
+        int c[][] = new int[3][3];
+        
+        for(int i = 0 ; i < 3 ; i++){
+            for (int j = 0 ; j < 3 ; j++) {
+                c[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        System.out.println("The additon of matrix A -> ");
+        
+        for(int i = 0 ; i < 3 ; i++){
+            System.out.println("");
+            for (int j = 0 ; j < 3 ; j++) {
+                System.out.print("\t " + c[i][j]);
+            }
+        }
+        
+        System.out.println("\nAnd Matrix B -> ");
+        for(int i = 0 ; i < 3 ; i++){
+            System.out.println("");
+            for (int j = 0 ; j < 3 ; j++) {
+                System.out.print("\t " + c[i][j]);
+            }
+        }
+
+        System.out.println("\nequals to -> ");
+                for(int i = 0 ; i < 3 ; i++){
+            System.out.println("");
+            for (int j = 0 ; j < 3 ; j++) {
+                System.out.print("\t " + c[i][j]);
+            }
+        }
+
     }
     
 }
