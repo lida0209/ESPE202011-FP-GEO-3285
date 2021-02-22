3d534334f08c58a00163f1e8daf45d4ce0548a0e
lida0209 <lidamk99@hotmail.com>
2021-02-22 13:04:18 -0500

    QUIZ_1

---
+++ Quiz/Unit2/Quiz1/src/quiz1/Quiz1.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class Quiz1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Student with average grades
+        int [] studentAverage;
+        String[] students = {"Mario", "Valentina", "Josue", "Noelia","Evelin"};        
+        float[] averageGrades = { 14.5F, 16.4F, 13.9F, 15.1F, 14.2F};
+        Scanner input = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the name of students from the average grades -> ");
+        n = input.nextInt();
+        
+         studentAverage = new int[n];
+         for (int i = 0; i < n; i++) {
+            System.out.print("Enter the number of students from the average grades " + (i + 1) + " -> ");
+            studentAverage[i] = input.nextInt();
+        
+            }
+
+        for (int student :studentAverage ) {
+            System.out.println("student Average grades " + student);
+        }
+        
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Average grades " + (i + 1) + " -> " + studentAverage[i]);
+            
+        }
+ 
+
+        for (String student : students) {
+            System.out.println("students names -> " + student);
+        }
+         
+        
+        for (int i = 0; i < averageGrades.length ; i++) {
+            System.out.println("Average of students " + (i + 1) + " -> " + averageGrades[i]);
+        }
+       
+      
+    }
+    
+}
